927ecde4254f4e2da40a3c3b979f285706b58278
moabcode <nombertoeduardo@gmail.com>
2026-07-28 12:42:18 -0500

    CABECERAAA

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -58,7 +58,7 @@ session_start();
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 
-<body class="hold-transition skin-green sidebar-mini">
+<body class="hold-transition skin-green sidebar-mini fixed">
 
     <?php
     
